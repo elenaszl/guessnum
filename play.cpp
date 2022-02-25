4c21c7ee72e13c63891c653874ce779244b30ae6
1102012宋梓菱 <96213795+elenaszl@users.noreply.github.com>
2022-02-25 13:49:18 +0800

    1102012宋梓菱

--- play.cpp
+++ play.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 using namespace std;
+
+void split(int n,int N[])
+{
+    N[0] = n / 1000;
+    N[1] = (n / 100) % 10;
+    N[2]= (n / 10) % 10;
+    N[3] = n % 10;
+}
+bool haszero(int n)
+{
+    int N[4];
+    split(n, N);
+    if (N[0] * N[1] * N[2] * N[3] == 0)
+        return true;
+    else
+        return false;
+}
+bool delete(int n)
+{
+    if(N[0]==N[1]||N[0]==N[2]||N[0]==N[3]||N[1]==N[2]||N[1]==N[3]||N[2]==N[3])
+       return true;
+    else 
+       return false;
+}
 int main()
 {
-  cout<<"student id"<<endl;
-  cout<<"name"<<endl;
+  
+  cout<<"1102012"<<endl;
+  cout<<"宋梓菱"<<endl;
   //-----------------------
   cout<<"demo"<<endl;
   return 1;
